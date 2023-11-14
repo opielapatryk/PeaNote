1b1f5945ad3ac61d3172bb88bb0f4b00194ec360
opielapatryk <xpatrykopiela@gmail.com>
2023-11-14 20:52:37 +0100

    git

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,6 +1,6 @@
 import { View, TextInput,Button } from 'react-native'
-import React, {useEffect, useState} from 'react'
-import {API_URL, useAuth} from '../context/AuthContext'
+import React, { useState} from 'react'
+import { useAuth} from '../context/AuthContext'
 import axios from 'axios'
 
 const Login = () => {
@@ -8,19 +8,10 @@ const Login = () => {
     const [password, setPassword] = useState('')
     const {onLogin} = useAuth()
 
-    useEffect(()=>{
-        const testCall = async()=>{
-            const result = await axios.get(`http://127.0.0.1:8000/api/users/`)
-            console.log(result);
-            
-        }
-        testCall()
-    },[])
-
     const login = async()=>{
         const result = await onLogin(email,password)
         if(result&&result.error){
-            alert(result.error)
+            console.log(result);
         }
     }
 
