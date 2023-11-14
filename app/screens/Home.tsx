c046debca5a0782c6cdd33cdf96f9f76a0278634
opielapatryk <xpatrykopiela@gmail.com>
2023-11-14 20:51:53 +0100

    render obj

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {Text, ScrollView } from 'react-native'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
 
 const Home = () => {
+  const [users,setUsers] = useState([])
+  useEffect(()=>{
+    const loadUser = async()=>{
+      try{
+        const result = await axios.get(`http://127.0.0.1:8000/api/users/`)
+        setUsers(result.data)
+      }catch(e){
+        alert(e.message)
+      }
+    }
+    loadUser()
+  },[])
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView>
+      {users.map((user)=>(
+        <Text key={user.id}>{user.email}</Text>
+      ))}
+    </ScrollView>
   )
 }
 
