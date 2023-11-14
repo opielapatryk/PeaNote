fa7a940784974826f3569c9727add0ede88a257e
opielapatryk <xpatrykopiela@gmail.com>
2023-11-14 12:15:12 +0100

    screens

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { View} from 'react-native';
 import React from 'react';
-import { styles } from './assets/styles';
-import { Note } from './components/Note';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BoardScreen from './components/BoardScreen'
+import FriendsScreen from './components/FriendsScreen'
+import SettingsScreen from './components/SettingsScreen'
+import AddFriendScreen from './components/AddFriendScreen'
+import SignInScreen from './components/SignInScreen'
+import SignUpScreen from './components/SignUpScreen'
 
+const Stack = createNativeStackNavigator();
+
+const getIsSignedIn = () => {
+  // custom logic
+  return false;
+};
 
 export default function App() {
+  const isSignedIn = getIsSignedIn();
   return (
-    <View style={styles.container}>
-      <Note text={'20;00 film'}/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      {isSignedIn ? (
+          <>
+            <Stack.Screen name="Board" component={BoardScreen} />
+            <Stack.Screen name="Friends" component={FriendsScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
+            <Stack.Screen name="AddFriend" component={AddFriendScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
-
