ad2758fcb737a9919dfd929d3b78173c72c2fef1
opielapatryk <xpatrykopiela@gmail.com>
2023-11-30 14:43:31 +0100

    resgister valiadtion

--- screens/public/Register.js
+++ screens/public/Register.js
@@ -4,27 +4,51 @@ import axios from 'axios'
 
 const Register = ({navigation}) => {
 
-  const [first_name, setFirstName] = useState()
-  const [last_name, setLastName] = useState()
-  const [email, setEmail] = useState()
-  const [password1, setPassword1] = useState()
-  const [password2, setPassword2] = useState()
+  const [first_name, setFirstName] = useState('')
+  const [last_name, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password1, setPassword1] = useState('')
+  const [password2, setPassword2] = useState('')
+  const [message, setMessage] = useState('')
 
   const createAccount = async () => {
-    try {
-      const result = await axios.post('http://localhost:8000/custom_register', {
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        password1: password1,
-        password2: password2
-      });
+    function isValidEmail(email) {
+      // Regular expression for basic email validation
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    }
 
-      console.log(result.status);
+    function isValidPassword(password) {
+      // Password must be at least 8 characters, contain at least one number, and one uppercase letter
+      const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+      return passwordRegex.test(password);
+    }
 
-      return result
-    } catch (error) {
-      console.log(error.message);
+    if(first_name === '' || last_name === '' || email === '' || password1 === '' || password2 === ''){
+      setMessage('All fields must be provided!..')
+    }else if(password1 != password2){
+      setMessage('Passwords must be the same!..')
+    }else if(!isValidEmail(email)){
+      setMessage('Email must correct!..')
+    }else if(!isValidPassword(password1)){
+      setMessage('Password must be at least 8 characters, contain at least one number and big character!..')
+    }else{
+      try {
+        const result = await axios.post('http://localhost:8000/custom_register', {
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          password1: password1,
+          password2: password2
+        });
+  
+        console.log(result.status);
+        setMessage('Account created successfuly!')
+        navigation.navigate('Login')
+        return result
+      } catch (error) {
+        console.log(error.message);
+      }
     }
   }
 
@@ -35,9 +59,9 @@ const Register = ({navigation}) => {
       <TextInput placeholder='email' onChangeText={setEmail} value={email}/>
       <TextInput secureTextEntry placeholder='password' onChangeText={setPassword1} value={password1} blurOnSubmit={false}/>
       <TextInput secureTextEntry placeholder='confirm password' onChangeText={setPassword2} value={password2} blurOnSubmit={false}/>
-
       <Button onPress={()=>createAccount()} title='Create Account'/>
       <Button onPress={()=>navigation.navigate('Login')} title='Sign In'/>
+      <Text>{message}</Text>
     </View>
   )
 }
