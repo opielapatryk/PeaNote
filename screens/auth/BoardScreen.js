0c23e48a2b23ea5568dcd3c5ec0c9b6e259eae87
opielapatryk <xpatrykopiela@gmail.com>
2023-12-05 22:10:47 +0100

    separate logic from comp boardscreen

--- screens/auth/BoardScreen.js
+++ screens/auth/BoardScreen.js
@@ -3,45 +3,14 @@ import {ScrollView,Pressable,View} from 'react-native';
 import {Note} from '../../components/Note'
 import { useDispatch, useSelector} from 'react-redux';
 import {styles} from '../../assets/styles';
-import {changeInfo,addNote, removeNote} from '../../store/boardSlice';
 import Menu from '../../components/Menu'
-import * as SecureStore from 'expo-secure-store';
-import axios from 'axios';
 import { useFocusEffect } from '@react-navigation/native';
-import {userLink} from '../../components/Constants'
+import { fetchNotes, checkThenChangeInfo } from './logic/api';
 
 const BoardScreen = ({navigation}) => {
     const { notes } = useSelector((state)=>state.board)
     const dispatch = useDispatch()
     const isMounted = useRef(true)
-
-    const loadNotes = async () => {
-      await notes.map((sticker) => dispatch(removeNote(sticker.id)));
-      try {
-        const userToken = await SecureStore.getItemAsync('userToken');
-        const currentUserId = await SecureStore.getItemAsync('userId');
-
-        const result = await axios.get(userLink(currentUserId),{
-          headers:{
-            Authorization: userToken,
-          }
-        })
-
-        const stickersRequest = result.data.stickersOnBoard.map(url =>
-          axios.get(url)
-          .then(response => response.data)
-        );
-
-        Promise.all(stickersRequest)
-        .then(stickersData=>{
-          stickersData.forEach(sticker => dispatch(addNote({id: sticker.id, text: sticker.content, isNote:true, isInfo:false})))
-        })
-        
-        return result
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
 
     useFocusEffect(
       React.useCallback(()=>{
@@ -52,7 +21,7 @@ const BoardScreen = ({navigation}) => {
     useFocusEffect(
       React.useCallback(()=>{
         if(isMounted.current){
-          loadNotes();
+          fetchNotes(dispatch,notes);
           isMounted.current = false
         }
       },[notes])
@@ -62,14 +31,7 @@ const BoardScreen = ({navigation}) => {
         <View>
           <Menu navigation={navigation}/>
           <Pressable
-              onPress={() => {
-                notes.map((note) => {
-                  if(note.isInfo === true)
-                  {
-                    dispatch(changeInfo(note.id))
-                  }
-                })
-              }}
+              onPress={() => checkThenChangeInfo(dispatch,notes)}
               style={styles.board}
             >
             <ScrollView>
---
+++ screens/auth/logic/api.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+import { userLink } from '../../../components/Constants';
+import {addNote,removeNote} from '../../../store/boardSlice';
+import {changeInfo} from '../../../store/boardSlice';
+
+export const fetchNotes = async (dispatch,notes) => {
+  await notes.map((sticker) => dispatch(removeNote(sticker.id)));
+  try {
+    const userToken = await SecureStore.getItemAsync('userToken');
+    const currentUserId = await SecureStore.getItemAsync('userId');
+
+    const result = await axios.get(userLink(currentUserId), {
+      headers: {
+        Authorization: userToken,
+      },
+    });
+
+    const stickersRequest = result.data.stickersOnBoard.map((url) =>
+      axios.get(url).then((response) => response.data)
+    );
+
+    Promise.all(stickersRequest).then((stickersData) => {
+      stickersData.forEach((sticker) =>
+        dispatch(addNote({ id: sticker.id, text: sticker.content, isNote: true, isInfo: false }))
+      );
+    });
+
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const checkThenChangeInfo = (dispatch, notes) => {
+    notes.map((note) => {
+      if(note.isInfo === true)
+      {
+        dispatch(changeInfo(note.id))
+      }
+    })
+  }
