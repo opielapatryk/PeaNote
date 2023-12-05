df020601cda52f1c56a050b56af47e6cdd307b0b
opielapatryk <xpatrykopiela@gmail.com>
2023-12-05 23:27:01 +0100

    pending screen separate logic

--- screens/auth/PendingScreen.js
+++ screens/auth/PendingScreen.js
@@ -3,11 +3,9 @@ import {ScrollView,Pressable,View,Text,Button} from 'react-native';
 import {Note} from '../../components/Note'
 import {styles} from '../../assets/styles';
 import { useDispatch, useSelector} from 'react-redux';
-import {changeInfo,addNote,removeNote} from '../../store/boardSlice';
-import * as SecureStore from 'expo-secure-store';
-import axios from 'axios';
+import {changeInfo} from '../../store/boardSlice';
 import { useFocusEffect } from '@react-navigation/native';
-import {userLink,stickerLink} from '../../components/Constants'
+import {loadPendingNotes, removeNotesFromReduxStore,sendNoteToBoard} from './logic/apiPendingScreen'
 
 const PendingScreen = () => {
     const { notes } = useSelector((state)=>state.board)
@@ -16,137 +14,42 @@ const PendingScreen = () => {
 
     useFocusEffect(
       React.useCallback(() => {
-        const loadPendingNotes = async () => {
-          try {
-            const userToken = await SecureStore.getItemAsync('userToken');
-            const currentUserId = await SecureStore.getItemAsync('userId');
-    
-            const result = await axios.get(userLink(currentUserId), {
-              headers: {
-                Authorization: userToken,
-              }
-            });
-    
-            const stickersRequest = result.data.pending.map(url =>
-              axios.get(url)
-                .then(response => response.data)
-            );
-    
-            const stickersData = await Promise.all(stickersRequest);
-            stickersData.forEach(sticker => console.log(JSON.stringify(sticker)));
-            stickersData.forEach(sticker => dispatch(addNote({ id: sticker.id, text: sticker.content, isNote: true, isInfo: false })));
-    
-            return result;
-          } catch (error) {
-            console.log(error.message);
-          }
-        };
-    
-        loadPendingNotes();
-    
+        loadPendingNotes(dispatch);
         return () => {
-          const removeNotesFromReduxStore = async (notes) => {
-            await notes.map((sticker) => dispatch(removeNote(sticker.id)));
-          };
-    
-          removeNotesFromReduxStore(notes);
+          removeNotesFromReduxStore(notes,dispatch);
         };
       }, [])
     );
 
-    async function sendNoteToBoard(stickerID){
-        try {
+    return (
+      <View>
+        <Pressable
+            onPress={() => {
+              notes.map((note) => {
+                if(note.isInfo === true)
+                {
+                  dispatch(changeInfo(note.id))
+                }
+              })
+            }}
+            style={styles.board}
+          >
+          <ScrollView>
+          {fetched && (
+              <View>
+                <Text>Note has been sent to the board successfully!</Text>
+              </View>
+            )}
+            {notes.map((note) => (
+              <View key={note.id}>
+                <Note id={note.id} text={note.text} isNote={note.isNote} isInfo={note.isInfo} />
+                <Button title='Approve note' onPress={()=>sendNoteToBoard(note.id,setFetched,notes,dispatch,)}/>
+              </View>
+            ))}
+          </ScrollView>
 
-            let userID = await SecureStore.getItemAsync('userId');
-
-            const resp = await axios.get(userLink(userID))
-    
-            let stickersOnBoard = resp.data.stickersOnBoard;
-            let pending = resp.data.pending;
-
-            stickersOnBoard.push(stickerLink(stickerID))
-
-            let newPendingArr = await pending.filter(sticker => sticker != stickerLink(stickerID))
-
-            const patchStickersOnBoardResp = await axios.patch(userLink(userID),{
-              'stickersOnBoard': stickersOnBoard
-            })
-
-            const patchPendingStickersResp = await axios.patch(userLink(userID),{
-              'pending': newPendingArr
-            })
-
-            if(patchStickersOnBoardResp.status === 200 && patchPendingStickersResp.status === 200){
-                setFetched(true)
-                const removeNotesFromReduxStore = async (notes) => {
-                  await notes.map((sticker) => dispatch(removeNote(sticker.id)));
-                };
-          
-                removeNotesFromReduxStore(notes);
-
-                const loadPendingNotes = async () => {
-                  try {
-                    const userToken = await SecureStore.getItemAsync('userToken');
-                    const currentUserId = await SecureStore.getItemAsync('userId');
-            
-                    const result = await axios.get(userLink(currentUserId), {
-                      headers: {
-                        Authorization: userToken,
-                      }
-                    });
-            
-                    const stickersRequest = result.data.pending.map(url =>
-                      axios.get(url)
-                        .then(response => response.data)
-                    );
-            
-                    const stickersData = await Promise.all(stickersRequest);
-                    stickersData.forEach(sticker => dispatch(addNote({ id: sticker.id, text: sticker.content, isNote: true, isInfo: false })));
-            
-                    return result;
-                  } catch (error) {
-                    console.log(error.message);
-                  }
-                };
-            
-                loadPendingNotes();
-            }
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-
-      return (
-        <View>
-          <Pressable
-              onPress={() => {
-                notes.map((note) => {
-                  if(note.isInfo === true)
-                  {
-                    dispatch(changeInfo(note.id))
-                  }
-                })
-              }}
-              style={styles.board}
-            >
-            <ScrollView>
-            {fetched && (
-                <View>
-                  <Text>Note has been sent to the board successfully!</Text>
-                </View>
-              )}
-              {notes.map((note) => (
-                <View key={note.id}>
-                  <Note id={note.id} text={note.text} isNote={note.isNote} isInfo={note.isInfo} />
-                  <Button title='Approve note' onPress={()=>sendNoteToBoard(note.id)}/>
-                </View>
-              ))}
-            </ScrollView>
-
-          </Pressable>
-        </View>
+        </Pressable>
+      </View>
       );
     };
-
-
 export default PendingScreen;
---
+++ screens/auth/logic/apiPendingScreen.js
@@ -0,0 +1,64 @@
+import * as SecureStore from 'expo-secure-store';
+import axios from 'axios';
+import {userLink,stickerLink} from '../../../components/Constants'
+import {addNote,removeNote} from '../../../store/boardSlice';
+
+export const loadPendingNotes = async (dispatch) => {
+    try {
+      const userToken = await SecureStore.getItemAsync('userToken');
+      const currentUserId = await SecureStore.getItemAsync('userId');
+
+      const result = await axios.get(userLink(currentUserId), {
+        headers: {
+          Authorization: userToken,
+        }
+      });
+
+      const stickersRequest = result.data.pending.map(url =>
+        axios.get(url)
+          .then(response => response.data)
+      );
+
+      const stickersData = await Promise.all(stickersRequest);
+      stickersData.forEach(sticker => dispatch(addNote({ id: sticker.id, text: sticker.content, isNote: true, isInfo: false })));
+
+      return result;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+export const removeNotesFromReduxStore = async (notes,dispatch) => {
+    await notes.map((sticker) => dispatch(removeNote(sticker.id)));
+  };
+
+export async function sendNoteToBoard(stickerID,setFetched,notes,dispatch,){
+    try {
+        let userID = await SecureStore.getItemAsync('userId');
+
+        const resp = await axios.get(userLink(userID))
+
+        let stickersOnBoard = resp.data.stickersOnBoard;
+        let pending = resp.data.pending;
+
+        stickersOnBoard.push(stickerLink(stickerID))
+
+        let newPendingArr = await pending.filter(sticker => sticker != stickerLink(stickerID))
+
+        const patchStickersOnBoardResp = await axios.patch(userLink(userID),{
+          'stickersOnBoard': stickersOnBoard
+        })
+
+        const patchPendingStickersResp = await axios.patch(userLink(userID),{
+          'pending': newPendingArr
+        })
+
+        if(patchStickersOnBoardResp.status === 200 && patchPendingStickersResp.status === 200){
+            setFetched(true)
+            removeNotesFromReduxStore(notes,dispatch);
+            loadPendingNotes(dispatch);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
