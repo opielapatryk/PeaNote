133f2d8700449cc3d118f234bc6eb2b35355eef7
opielapatryk <xpatrykopiela@gmail.com>
2023-12-06 00:16:31 +0100

    note separate logic

--- components/Note.js
+++ components/Note.js
@@ -2,49 +2,15 @@ import { Text, View, Pressable } from 'react-native';
 import React from 'react';
 import { styles } from '../assets/styles';
 import { useDispatch, useSelector} from 'react-redux';
-import {changeInfo,removeNote} from '../store/boardSlice'
-import { stickerLink } from './Constants';
-import axios from 'axios'
+import { handlePress } from './logic/apiNote';
 
 export function Note({ id, text, isNote, isInfo }) {
   const { notes } = useSelector((state) => state.board);
   const dispatch = useDispatch();
 
-  const deleteNote = async () => {
-    try {
-      const result = await axios.delete(stickerLink(id))
-      return result
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  const handlePress = () => {
-    {notes.forEach(note => {
-      if(note.isInfo === true){
-        dispatch(changeInfo(note.id));
-      }
-    });}
-    if (isInfo) {
-      dispatch(changeInfo(id));
-      dispatch(removeNote(id));
-      deleteNote();
-    } else {
-      dispatch(changeInfo(id));
-    }
-  };
-
-  if (isNote && isInfo) {
+  if (isNote) {
     return (
-      <Pressable style={styles.note} onPress={handlePress}>
-        <View>
-          <Text>{notes.find((item) => item.id === id)?.text}</Text>
-        </View>
-      </Pressable>
-    );
-  } else if (isNote && !isInfo) {
-    return (
-      <Pressable style={styles.note} onPress={handlePress}>
+      <Pressable style={styles.note} onPress={()=>handlePress(notes,dispatch,isInfo,id)}>
         <View>
           <Text>{notes.find((item) => item.id === id)?.text}</Text>
         </View>
---
+++ components/logic/apiNote.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import {changeInfo,removeNote} from '../../store/boardSlice'
+import { stickerLink } from '../Constants';
+
+export const handlePress = (notes,dispatch,isInfo,id) => {
+    {notes.forEach(note => {
+        if(note.isInfo === true){
+        dispatch(changeInfo(note.id));
+        }
+    });}
+    if (isInfo) {
+        dispatch(changeInfo(id));
+        dispatch(removeNote(id));
+        deleteNote(id);
+    } else {
+        dispatch(changeInfo(id));
+    }
+};
+
+const deleteNote = async (id) => {
+    try {
+      const result = await axios.delete(stickerLink(id))
+      return result
+    } catch (error) {
+      console.log(error.message);
+    }
+}
+
