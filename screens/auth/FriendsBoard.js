ad589b1a980e27f8a52e7a01b5cbf544e6540365
opielapatryk <xpatrykopiela@gmail.com>
2023-12-05 17:28:08 +0100

    fix friendsBoard bug

--- screens/auth/FriendsBoard.js
+++ screens/auth/FriendsBoard.js
@@ -1,94 +1,79 @@
-import { Text,TextInput,View,Button } from 'react-native'
-import React,{useState} from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import { Text, TextInput, View, Button } from 'react-native';
+import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
-import {userLink,stickersLink,stickerLink} from '../../components/Constants'
+import { userLink, stickersLink, stickerLink } from '../../components/Constants';
 
-export default FriendsBoard = ({ route,navigation }) => {
-    const { friendId, friendName } = route.params;
-    const [content,setContent] = useState('');
-    const [message, setMessage] = useState('');
+const FriendsBoard = ({ route, navigation }) => {
+  const { friendId, friendName } = route.params;
+  const [content, setContent] = useState('');
+  const [message, setMessage] = useState('');
 
-    const createNote = async () => {
-        try {
-            currentUserId = await SecureStore.getItemAsync('userId');
-            const userURL = userLink(currentUserId)
-            let result;
+  const createNote = async () => {
+    try {
+      const currentUserId = await SecureStore.getItemAsync('userId');
+      const userURL = userLink(currentUserId);
 
-            if(content != ''){
-                result = await axios.post(stickersLink,{
-                    'content':content,
-                    'creator':userURL
-                })
-            }else{
-                setMessage('Note cannot be empty..')
-            }
-            
+      if (content !== '') {
+        const result = await axios.post(stickersLink(), {
+          content: content,
+          creator: userURL,
+        });
 
-            const stickerID = result.data.id
+        const stickerID = result.data.id;
+        const friendURL = userLink(friendId);
+        const resultStickers = await axios.get(friendURL);
 
-            const friendURL = userLink(friendId)
+        const listKey = resultStickers.data.askBeforeStick ? 'pending' : 'stickersOnBoard';
+        let list = resultStickers.data[listKey];
+        list.push(stickerLink(stickerID));
 
-            const resultStickers = await axios.get(friendURL)
+        await axios.patch(userLink(friendId), {
+          [listKey]: list,
+        });
 
-            if(resultStickers.data.askBeforeStick){
-                let list = resultStickers.data.pending
-
-                list.push(stickerLink(stickerID));
-
-                await axios.patch(userLink(friendId),{
-                'pending': list
-                })
-
-            }else{
-                let list = resultStickers.data.stickersOnBoard
-    
-                list.push(stickerLink(stickerID));
-    
-                await axios.patch(userLink(friendId),{
-                    'stickersOnBoard': list
-                })
-            }
-
-            if(result.status === 201){
-                setMessage('Note created successfully!')
-                setContent('')
-            }   
-        } catch (error) {
-            console.log(error.message);
-            setMessage('Cannot create note, something went wrong..')
+        if (result.status === 201) {
+          setMessage('Note created successfully!');
+          setContent('');
         }
+      } else {
+        setMessage('Note cannot be empty..');
+      }
+    } catch (error) {
+      console.log(error.message);
+      setMessage('Cannot create note, something went wrong..');
     }
+  };
 
-    const removeFriend = async () => {
-        try {
-            currentUserId = await SecureStore.getItemAsync('userId');
-            const currentUserResult = await axios.get(userLink(currentUserId))
+  const removeFriend = async () => {
+    try {
+      const currentUserId = await SecureStore.getItemAsync('userId');
+      const currentUserResult = await axios.get(userLink(currentUserId));
 
-            let list = currentUserResult.data.friends
+      const list = currentUserResult.data.friends.filter((element) => element !== userLink(friendId));
 
-            list = list.filter((element) => element !== userLink(friendId))
+      const resp = await axios.patch(userLink(currentUserId), {
+        friends: list,
+      });
 
-            const resp = await axios.patch(userLink(currentUserId),{
-                'friends':list
-            })
-
-            if(resp.status === 200) {
-                navigation.navigate('Friends')
-            }
-            return resp
-        } catch (error) {
-            console.log(error.message);
-        }
+      if (resp.status === 200) {
+        navigation.navigate('Friends');
+      }
+      return resp;
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-    return (
-      <View>
-        <Button title='remove friend' onPress={()=>removeFriend()}/>
-        <Text>Welcome on {friendName?friendName:'your friend'} board</Text>
-        <TextInput placeholder='note.....' value={content} onChangeText={(content)=>setContent(content)}/>
-        <Button title='create note' onPress={()=>createNote()}/>
-        <Text>{message}</Text>
-      </View>
-    );
-}
+  return (
+    <View>
+      <Button title="Remove Friend" onPress={removeFriend} />
+      <Text>Welcome to {friendName ? friendName + "'s" : 'your'} board</Text>
+      <TextInput placeholder="Note....." value={content} onChangeText={(text) => setContent(text)} />
+      <Button title="Create Note" onPress={createNote} />
+      <Text>{message}</Text>
+    </View>
+  );
+};
+
+export default FriendsBoard;
