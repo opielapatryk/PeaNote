8540e7f6d7c2f038f821c816016a87580e0811c3
opielapatryk <xpatrykopiela@gmail.com>
2023-12-15 13:38:32 +0100

    Oauth2 google works

--- screens/public/LoginFB.js
+++ screens/public/LoginFB.js
@@ -1,9 +1,15 @@
 import { View,TextInput,Button,Text } from 'react-native'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { styles } from '../../assets/styles/styles';
 import { FIREBASE_AUTH,FIREBASE_DB } from '../../FIrebaseConfig';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
+import {
+  GoogleOneTapSignIn,
+  statusCodes,
+  OneTapUser,
+  GoogleSignin,
+} from '@react-native-google-signin/google-signin';
 
 const LoginFB = ({navigation}) => {
   const [first_name, setFirstName] = useState('');
@@ -12,6 +18,10 @@ const LoginFB = ({navigation}) => {
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const auth = FIREBASE_AUTH
+  const [myuserInfo,setUserInfo] = useState('');
+  const [userError,setUserError] = useState('');
+
+
 
   const signIn = async () =>{
     try {
@@ -40,6 +50,33 @@ const LoginFB = ({navigation}) => {
         console.log(error);
     }
   }
+
+  const configureGoogleSignIn = () =>{
+    GoogleSignin.configure({
+      webClientId:"1088925346926-il03fpvr7b6q4qb5llrofkafd7c292dr.apps.googleusercontent.com",
+      iosClientId:"1088925346926-3e883fbcli5qkc14vibgq3c2i3rl1735.apps.googleusercontent.com"
+    })
+  }
+
+  useEffect(()=>{
+    configureGoogleSignIn()
+  })
+
+  const signInGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn();
+      setUserInfo(userInfo);
+    } catch (error) {
+      setUserError(error);
+    }
+  };
+
+  const logoutGoogle = async () => {
+    setUserInfo(undefined)
+    GoogleSignin.revokeAccess()
+    GoogleSignin.signOut()
+  }
   return (
     <View style={styles.container}>
       <TextInput placeholder='first_name' onChangeText={setFirstName} value={first_name}/>
@@ -48,7 +85,10 @@ const LoginFB = ({navigation}) => {
       <TextInput placeholder='password' secureTextEntry onChangeText={setPassword} value={password}/>   
       <Button onPress={signIn} title='sign in' />
       <Button onPress={signUp} title='Create Account' />
+      <Button onPress={signInGoogle} title='signInGoogle'/>
       <Text>{message}</Text>
+      <Text>{JSON.stringify(myuserInfo.user)}</Text>
+      <Text>{JSON.stringify(userError)}</Text>
     </View>
   );
 }
