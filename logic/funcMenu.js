de94272895c3a717889741604ceb65ad064d6097
opielapatryk <xpatrykopiela@gmail.com>
2023-12-28 16:08:59 +0100

    remove notes from redux store after logout

--- logic/funcMenu.js
+++ logic/funcMenu.js
@@ -4,8 +4,8 @@ import { GoogleSignin} from '@react-native-google-signin/google-signin';
 
 export const signOutAndClearReduxStore = async (notes,dispatch_redux,pendingNotes) => {
     try {
-        // notes.forEach(sticker => dispatch_redux(removeNote(sticker.id)));
-        // pendingNotes.forEach(sticker => dispatch_redux(removePendingNote(sticker.id)));
+        notes.forEach(sticker => dispatch_redux(removeNote(sticker.id)));
+        pendingNotes.forEach(sticker => dispatch_redux(removePendingNote(sticker.id)));
         await auth().signOut();
         await GoogleSignin.revokeAccess();
     } catch (error) {
