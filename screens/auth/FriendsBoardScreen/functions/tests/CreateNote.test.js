6ecda26c032e0c13996de59d87e82ef514cc6378
opielapatryk <xpatrykopiela@gmail.com>
2024-01-23 09:22:15 +0100

    Create Note test.

--- screens/auth/FriendsBoardScreen/functions/tests/CreateNote.test.js
+++ screens/auth/FriendsBoardScreen/functions/tests/CreateNote.test.js
@@ -1,3 +1,28 @@
-it('Set message if note created.', () => {
-    
+import { createNote } from '../createNote';
+
+jest.mock('@react-native-firebase/auth', () => () => {
+    return {
+        currentUser: {
+            email:'test@example.com'
+        }
+    }
+  })
+
+jest.mock('@react-native-firebase/firestore', () => ({
+  default: jest.fn(),
+}));
+
+describe('createNote function', () => {
+  it('should set message when content is empty', async () => {
+    const setContent = jest.fn();
+    const setMessage = jest.fn();
+    const friendEmail = 'friend@example.com';
+
+    // Call the function with empty content
+    await createNote('', setContent, setMessage, friendEmail);
+
+    // Assertions
+    expect(setContent).not.toHaveBeenCalled();
+    expect(setMessage).toHaveBeenCalledWith('Note cannot be empty..');
+  });
 });
