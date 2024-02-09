9bca4c1acad9f1d0a40692aacee5240cdc52318b
opielapatryk <xpatrykopiela@gmail.com>
2024-02-09 14:48:48 +0100

    Image compression

--- screens/auth/SettingsScreen/components/SettingsScreen.js
+++ screens/auth/SettingsScreen/components/SettingsScreen.js
@@ -17,7 +17,8 @@ import * as ImagePicker from 'expo-image-picker'
 import auth, { firebase } from '@react-native-firebase/auth';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import firestore from '@react-native-firebase/firestore';
-import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
+import * as ImageManipulator from 'expo-image-manipulator';
+
 
 const SettingsScreen = () => {
   const [askBeforeStickingNoteFlag, setAskBeforeStickingNoteFlag] = useState(false);
@@ -71,17 +72,18 @@ const uploadImage = async () => {
 
   const { uri } = image;
 
-  const compressedImageData = (await manipulateAsync(uri,[{resize:{
+
+  const compressedImageData = (await ImageManipulator.manipulateAsync(uri,[{resize:{
     height: 300, 
     width: 300
-  }}],{compress:0.1,format: SaveFormat.JPEG}))
+  }}],{compress:0.1,format: ImageManipulator.SaveFormat.JPEG}))
 
   const path = `${EMAIL}`;
 
   const reference = firebase.storage().ref(path);
 
   try {
-    const response = await fetch(uri);
+    const response = await fetch(compressedImageData.uri);
     const blob = await response.blob();
 
     await reference.put(blob);
