ff8ca1b0157578d2c221ecce317f242224b0ceee
opielapatryk <xpatrykopiela@gmail.com>
2024-02-14 14:08:13 +0100

    Screens rendering tests

--- __tests__/screens/PendingScreen.test.js
+++ __tests__/screens/PendingScreen.test.js
@@ -1,46 +1,46 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import { Provider } from 'react-redux';  
-// import store from '../../../../../store/store'; 
-// import PendingScreen from '../PendingScreen';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { act } from 'react-test-renderer';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';  
+import store from '../../src/store/store'; 
+import PendingScreen from '../../src/screens/auth/PendingScreen/components/PendingScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { act } from 'react-test-renderer';
+import { SafeAreaProvider} from 'react-native-safe-area-context';
 
-// jest.mock('@react-native-firebase/firestore', () =>()=> {
-//   return {
-//     collection: jest.fn(()=>{
-//       return{
-//         where: jest.fn(()=>{
-//           return{
-//             get: jest.fn(() => Promise.resolve(true))
-//           }
-//         }),
-//       }
-//     }),
-//   };
-// })
-
-// jest.mock('@react-native-firebase/auth', () => ()=> {
-//   return {
-//     currentUser: {
-//       email:'test@example.com'
-//     }
-//   };
-// })
-
-// test('Pending screen renders correctly.', async () =>  {
-//   const tree = renderer.create(
-//     <Provider store={store}>
-//         <NavigationContainer>
-//             <PendingScreen />
-//         </NavigationContainer>
-//     </Provider>
-//   ).toJSON();
-
-//   await act(async () => {
-//     expect(tree).toMatchSnapshot();
-//   })
-// });
-test('fake test', () => {
-  expect(true).toBeTruthy();
+jest.mock('@react-native-firebase/firestore', () =>()=> {
+  return {
+    collection: jest.fn(()=>{
+      return{
+        where: jest.fn(()=>{
+          return{
+            get: jest.fn(() => Promise.resolve(true))
+          }
+        }),
+      }
+    }),
+  };
 })
+
+jest.mock('@react-native-firebase/auth', () => ()=> {
+  return {
+    currentUser: {
+      email:'test@example.com'
+    }
+  };
+})
+
+test('Pending screen renders correctly.', async () =>  {
+  const tree = renderer.create(
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+            <PendingScreen />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
+  ).toJSON();
+
+  await act(async () => {
+    expect(tree).toMatchSnapshot();
+  })
+});
