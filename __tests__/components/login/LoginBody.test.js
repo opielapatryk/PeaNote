62ad7074dc515a4c77219b10a277d208f2aead0a
opielapatryk <xpatrykopiela@gmail.com>
2024-02-14 13:05:38 +0100

    Login body test component test

---
+++ __tests__/components/login/LoginBody.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';  
+import store from '../../../src/store/store'
+import LoginBody from '../../../src/screens/login/components/LoginBody';
+import {act} from 'react-dom/test-utils'
+
+jest.mock('@react-native-firebase/auth', () => ({
+    auth: {
+        signInWithEmailAndPassword: jest.fn(),
+        createUserWithEmailAndPassword: jest.fn(),
+        signOut: jest.fn(),
+    },
+  }));
+  
+jest.mock('@react-native-google-signin/google-signin', () => ({
+    GoogleSignin: {
+        signIn: jest.fn(),
+    },
+  }));
+
+jest.mock('@react-native-firebase/firestore', () => ({
+    firestore: {
+        collection: jest.fn(),
+        doc: jest.fn(),
+        get: jest.fn(),
+    },
+}));
+
+test('Login body renders correctly.', async () => {
+
+  const tree = renderer.create(
+    <Provider store={store}>
+      <LoginBody />
+    </Provider>
+  ).toJSON();
+
+  await act(async() => {
+    expect(tree).toMatchSnapshot();
+  })
+});
