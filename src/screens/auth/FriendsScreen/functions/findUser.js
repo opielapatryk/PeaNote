1fb4324f81c64f1d1ed687c20b858c452e9860c8
opielapatryk <xpatrykopiela@gmail.com>
2024-02-14 16:59:25 +0100

    Search for users dont care about case sensivity

--- src/screens/auth/FriendsScreen/functions/findUser.js
+++ src/screens/auth/FriendsScreen/functions/findUser.js
@@ -4,72 +4,42 @@ import { Keyboard } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import { setFriendimage } from '../../../../store/settings/settingsSlice';
 import { downloadImage } from '../../BoardScreen/functions/downloadImage';
+import { searchForFriendId } from './searchForFriendId'
 
 export const findUser = async (dispatch, friendEmailOrUsername,navigation) => {
   const currentUserEmail = auth().currentUser.email;
 
-  const getUserDetails = async (email) => {
-    const userSnapshot = await firestore()
-      .collection('users')
-      .where('email', '==', email)
-      .get();
+  const friend = await searchForFriendId(friendEmailOrUsername);
 
-      if (userSnapshot.empty) {
-        const usernameSnapshot = await firestore()
-          .collection('users')
-          .where('username', '==', email)
-          .get();
-    
-        if (usernameSnapshot.empty) {
-          dispatch(setMessage(errorMessage));
-          dispatch(setEmail(''));
-          Keyboard.dismiss();
-          setTimeout(() => {
-            dispatch(setMessage(''));
-          }, 2000);
-          return null;
-        }
-        return usernameSnapshot.docs[0];
-      }
+  const currentUser = await firestore()
+  .collection('users')
+  .where('email', '==', currentUserEmail)
+  .get()
 
-    return userSnapshot.docs[0];
-  };
-
-  const currentUserDoc = await getUserDetails(currentUserEmail);
-
-  if (!currentUserDoc) {
-    return;
-  }
-
+  const currentUserDoc = currentUser.docs[0]
 
   const currentUserUsername = currentUserDoc.data().username
 
   const currentUserFriends = currentUserDoc.data().friends;
 
-  const isFriend = currentUserFriends.some(friend => 
-    friend.email === friendEmailOrUsername || friend.username === friendEmailOrUsername
-  );
+  const isFriend = currentUserFriends.some(myfriend => 
+    myfriend.email === friend.email);
 
   if (isFriend) {
     dispatch(setMessage('YOU ARE FRIENDS ALREADY'));
   } else {
-    const friendDoc = await getUserDetails(friendEmailOrUsername, 'USER NOT FOUND');
-
-    if (!friendDoc) {
-      return;
+    if (!friend) {
+      dispatch(setMessage('USER NOT FOUND'));
+      console.log('user not found');
     }
-
-    if (friendEmailOrUsername === currentUserEmail || friendEmailOrUsername === currentUserUsername) {
+    if (friend.email === currentUserEmail) {
       dispatch(setMessage('YOU CANNOT ADD YOURSELF TO FRIENDS'));
+      console.log('YOU CANNOT ADD YOURSELF TO FRIENDS');
     } else {
-      const friendemail = friendDoc.data().email
-      const friendusername = friendDoc.data().username
-
-      await downloadImage(friendemail).then((fileUri)=>{
-        dispatch(setFriendimage(fileUri));
-      })
-      
-      navigation.navigate('UserBoard', {name:friendusername, friendEmail: friendemail, oldnickname:''})
+      console.log('friend searched');
+      const fileUri = await downloadImage(friend.email)
+      dispatch(setFriendimage(fileUri));
+      navigation.navigate('UserBoard', {name:friend.username, friendEmail: friend.email, oldnickname:''})
     }
   }
 
