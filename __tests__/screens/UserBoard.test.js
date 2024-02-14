99c414dc40dc9b4ce33bad66b580c39b6198221c
opielapatryk <xpatrykopiela@gmail.com>
2024-02-14 14:08:51 +0100

    snapshots

---
+++ __tests__/screens/UserBoard.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';  
+import store from '../../src/store/store'; 
+import { SafeAreaProvider} from 'react-native-safe-area-context';
+import UserBoard from '../../src/screens/auth/UserBoardScreen/components/UserBoard';
+
+jest.mock('@react-native-firebase/auth', () => ({
+    signInWithEmailAndPassword: jest.fn(),
+    createUserWithEmailAndPassword: jest.fn(),
+    signOut: jest.fn(),
+}));
+
+jest.mock('@react-native-google-signin/google-signin', () => ({
+    signIn: jest.fn(),
+}));
+
+
+jest.mock('@invertase/react-native-apple-authentication', () => ({
+  AppleButton: jest.fn(),
+}));
+
+jest.mock('@react-native-firebase/firestore', () => ({
+    collection: jest.fn(),
+    doc: jest.fn(),
+    get: jest.fn(),
+}));
+
+
+test('Login screen renders correctly.', () => {
+  const tree = renderer.create(
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <UserBoard />
+      </SafeAreaProvider>
+    </Provider>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
