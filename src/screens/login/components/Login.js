fc8bd472f4b2de45edac539eaf99ada125716925
opielapatryk <xpatrykopiela@gmail.com>
2024-02-14 13:50:32 +0100

    Refactor login screen

--- src/screens/login/components/Login.js
+++ src/screens/login/components/Login.js
@@ -1,37 +1,14 @@
 import { View,Keyboard,TouchableWithoutFeedback,Image } from 'react-native'
-import React,{ useEffect } from 'react';
+import React from 'react';
 import { styles } from '../../../../assets/styles/styles';
 import LoginBody from './LoginBody';
 import CreateAccountBody from './CreateAccountBody';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeNote,removePendingNote } from '../../../store/notes/boardSlice';
-import { useFocusEffect } from '@react-navigation/native';
-import { setMessage,setEmail } from '../../../store/login/loginSlice';
+import { useSelector } from 'react-redux';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Login = () => {
-  const { notes,pendingNotes } = useSelector((state)=>state.board)
   const { createAccount } = useSelector((state)=>state.login)
-  const dispatch = useDispatch()
   const insets = useSafeAreaInsets();
-
-  useEffect(() => {
-    notes.forEach(note => {
-      dispatch(removeNote(note.id))
-    });
-    pendingNotes.forEach(note => {
-      dispatch(removePendingNote(note.id))
-    });
-  }, []);
-
-  useFocusEffect(
-    React.useCallback(() => {
-      return ()=>{
-        dispatch(setMessage(''))
-        dispatch(setEmail(''))
-      }
-    }, [])
-  );
 
   return (
     <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
