bc7d3f0b9239ae0538b2dcd90d6259b179bcca2a
opielapatryk <xpatrykopiela@gmail.com>
2024-02-20 11:59:13 +0100

    Remove unused apple sign in function

--- __tests__/components/login/LoginWithAppleButton.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import LoginWithAppleButton from '../../../src/screens/login/components/LoginWithAppleButton';
-
-test('LoginWithApple button renders correctly.', () => {
-  const tree = renderer.create(<LoginWithAppleButton />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
