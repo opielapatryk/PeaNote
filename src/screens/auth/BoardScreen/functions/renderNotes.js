5a323fa29f8cee0f971df803ba50f321f92b56f9
opielapatryk <xpatrykopiela@gmail.com>
2024-02-20 17:31:53 +0100

    Nickname is available on pending note

--- src/screens/auth/BoardScreen/functions/renderNotes.js
+++ src/screens/auth/BoardScreen/functions/renderNotes.js
@@ -1,7 +1,34 @@
 import {Note} from '../components/Note'
+import firestore from '@react-native-firebase/firestore'
+import auth, {firebase} from '@react-native-firebase/auth'
 
 export const renderNotes = ({item}) => {
+  let nick 
+
+  const getNickname = async () => {
+    const EMAIL = auth().currentUser.email
+    const currentUser = await firestore().collection('users').where('email', '==', EMAIL).get()
+  
+    if (currentUser.docs && currentUser.docs.length > 0) {
+      const friend = currentUser.docs[0].data().friends.find(
+        (friend) => friend.username === item.creator || friend.email === item.creator
+      );
+  
+      if (friend === undefined) {
+        return item.creator;
+      }
+      
+      return friend.nickname;
+    } else {
+      return item.creator; // Handle the case where docs array is undefined or empty
+    }
+  }
+
+  getNickname().then((nickname)=>{
+    nick = nickname
+  })
+
     return (
-      <Note id={item.id} isInfo={item.isInfo} content={item.text} creator={item.creator} />
+      <Note id={item.id} isInfo={item.isInfo} content={item.text} creator={nick?nick:item.creator} />
     )
   }
--- src/screens/auth/PendingScreen/components/PendingNote.js
+++ src/screens/auth/PendingScreen/components/PendingNote.js
@@ -6,19 +6,46 @@ import { handlePress } from '../functions/funcPendingNote';
 import { sendNoteToBoard } from '../functions/sendNoteToBoard';
 import { deleteNote } from '../functions/deleteNote';
 import { removePendingNote} from '../../../../store/notes/boardSlice';
-
+import firestore from '@react-native-firebase/firestore'
+import auth, {firebase} from '@react-native-firebase/auth'
 
 export function PendingNote({ id, isInfo, content,creator }) {
   const {pendingNotes} = useSelector((state) => state.board);
   const dispatch = useDispatch();
   const [isLoadingLeft,setLoadingLeft] = useState(false)
   const [isLoadingRight,setLoadingRight] = useState(false)
+  const [nickname, setNickname] = useState()
+
+  const getNickname = async () => {
+    const EMAIL = auth().currentUser.email
+    const currentUser = await firestore().collection('users').where('email', '==', EMAIL).get()
+  
+    if (currentUser.docs && currentUser.docs.length > 0) {
+      const friend = currentUser.docs[0].data().friends.find(
+        (friend) => friend.username === creator || friend.email === creator
+      );
+  
+      if (friend === undefined) {
+        return creator;
+      }
+  
+      return friend.nickname;
+    } else {
+      return creator; // Handle the case where docs array is undefined or empty
+    }
+  }
+  
+    getNickname().then((nickname)=>{
+      setNickname(nickname)
+    })
 
   return (
     <Pressable onPress={()=>handlePress(pendingNotes,dispatch,isInfo,id)}>
     {!isInfo&&<>
       <View style={styles.note}>
-      <Text style={styles.noteTextHeader}>{pendingNotes.find((item) => item.id === id)?.creator}</Text>
+      <Text style={styles.noteTextHeader}>
+        {nickname?nickname:pendingNotes.find((item) => item.id === id)?.creator}
+        </Text>
         <Text style={styles.noteText}>{pendingNotes.find((item) => item.id === id)?.text}</Text>
       </View>
 
