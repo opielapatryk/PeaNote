8f2eef5c832cf4836f4aca1703a8f45d2d73c296
opielapatryk <xpatrykopiela@gmail.com>
2024-02-21 18:02:42 +0100

    Auth linked, apple, google, email

--- src/screens/login/functions/appleSignin.js
+++ src/screens/login/functions/appleSignin.js
@@ -3,23 +3,30 @@ import { appleAuth } from '@invertase/react-native-apple-authentication';
 import firestore from '@react-native-firebase/firestore';
 
 export const appleSignin = async ()=>{
-    const appleAuthRequestResponse = await appleAuth.performRequest({
+      let appleAuthRequestResponse = await appleAuth.performRequest({
         requestedOperation:appleAuth.Operation.LOGIN,
         requestedScopes:[appleAuth.Scope.FULL_NAME,appleAuth.Scope.EMAIL],
       })
-  
-      const { identityToken, nonce } = appleAuthRequestResponse;
+      let { identityToken, nonce } = appleAuthRequestResponse;
+      let appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce)
 
-      const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce)
-  
-      const user_sign_in = await auth().signInWithCredential(appleCredential);
+      let user_sign_in
+      
+      try {
+        user_sign_in = await auth().signInWithEmailAndPassword('xpatrykopiela@gmail.com', 'Oopiela007');
+
+      } catch (error) {
+        user_sign_in = await auth().createUserWithEmailAndPassword('xpatrykopiela@gmail.com', 'Oopiela007');
+      }
+
+      auth().currentUser.linkWithCredential(appleCredential)
 
       if(user_sign_in.additionalUserInfo.isNewUser){
         firestore()
         .collection('users')
         .add({
-          email: user_sign_in.user.email,
-          username:user_sign_in.user.email,
+          email: user.user.email,
+          username:user.user.email,
           friends: [],
           friends_requests: [],
           pending_requests: [],
