a526c1c1f2b26ce1326eb573eff9cab31ba25aac
opielapatryk <xpatrykopiela@gmail.com>
2024-02-22 10:58:53 +0100

    Add store provider to LoginWithGoogle test

--- __tests__/components/login/LoginWithGoogleButton.test.js
+++ __tests__/components/login/LoginWithGoogleButton.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import LoginWithGoogleButton from '../../../src/screens/login/components/LoginWithGoogleButton';
+import { Provider } from 'react-redux';  
+import store from '../../../src/store/store'
 
 jest.mock('@react-native-firebase/auth', () => ({
     auth: {
@@ -25,6 +27,10 @@ jest.mock('@react-native-google-signin/google-signin', () => ({
 }));
   
 test('LoginWithGoogle button renders correctly.', () => {
-  const tree = renderer.create(<LoginWithGoogleButton />).toJSON();
+  const tree = renderer.create(
+    <Provider store={store}>
+      <LoginWithGoogleButton />
+    </Provider>
+      ).toJSON();
   expect(tree).toMatchSnapshot();
 });
