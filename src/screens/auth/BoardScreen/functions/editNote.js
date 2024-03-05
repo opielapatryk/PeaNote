b9ddb9fd7b487cfeb8de3ea6fbe481bd6e7209d4
opielapatryk <xpatrykopiela@gmail.com>
2024-03-05 13:33:42 +0100

    Edit note bug fixed

--- src/screens/auth/BoardScreen/functions/editNote.js
+++ src/screens/auth/BoardScreen/functions/editNote.js
@@ -1,29 +1,54 @@
 import auth from '@react-native-firebase/auth';
 import { firebase } from '@react-native-firebase/database';
 
-export const editNote = async (newContent, oldContent, creator) => {
-  const EMAIL = auth().currentUser.email;
-
+const findUserByEmail = async (email) => {
   const usersRef = firebase.app().database('https://stickify-407810-default-rtdb.europe-west1.firebasedatabase.app/').ref('users');
+  const snapshot = await usersRef.orderByChild('email').equalTo(email).once('value');
+  
+  return snapshot.val();
+};
 
+const findNoteByContentAndCreator = async (notesRef, oldContent, creator) => {
+  const snapshot = await notesRef.orderByChild('content').equalTo(oldContent).once('value');
+  const notes = snapshot.val();
+
+  if (notes) {
+    const noteIds = Object.keys(notes);
+
+    for (const noteId of noteIds) {
+      const note = notes[noteId];
+
+      // Add logging for debugging
+      console.log('Found note:', note);
+
+      // Check if note exists and has the creator property
+      if (note && note.creator === creator) {
+        return { noteId, note };
+      }
+    }
+  }
+
+  return null;
+};
+
+export const editNote = async (newContent, oldContent, creator) => {
   try {
-    const snapshot = await usersRef.orderByChild('email').equalTo(EMAIL).once('value');
+    const currentUserEmail = auth().currentUser.email;
+    const userData = await findUserByEmail(currentUserEmail);
 
-    if (snapshot.exists()) {
-      const userData = snapshot.val();
+    if (userData) {
       const userId = Object.keys(userData)[0];
+      const notesRef = firebase.app().database('https://stickify-407810-default-rtdb.europe-west1.firebasedatabase.app/').ref(`users/${userId}/notes`);
 
-      // EDIT NOTE IN REALTIME DATABASE
-      await usersRef.child(`${userId}/notes`).orderByChild('content').equalTo(oldContent).once('value', (snapshot) => {
-        snapshot.forEach((childSnapshot) => {
-          const note = childSnapshot.val();
+      const noteData = await findNoteByContentAndCreator(notesRef, oldContent, creator);
 
-          // Check if the note has the matching creator
-          if (note.creator === creator) {
-            childSnapshot.ref.update({ content: newContent });
-          }
-        });
-      });
+      if (noteData) {
+        const { noteId } = noteData;
+        await notesRef.child(`${noteId}/content`).set(newContent);
+        console.log('Note edited successfully!');
+      } else {
+        console.warn('Note not found or creator does not match.');
+      }
     } else {
       console.warn('User not found.');
     }
